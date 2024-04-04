f48088268d1f4056bfa8ab86a922721d77aac1c0
Alex <alewilliam789@gmail.com>
2024-04-04 11:07:33 -0700

    Map the rss values to the article gateway

--- provenance/components/endpoints/src/main/java/io/collective/endpoints/EndpointWorker.java
+++ provenance/components/endpoints/src/main/java/io/collective/endpoints/EndpointWorker.java
@@ -1,13 +1,17 @@
 package io.collective.endpoints;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import io.collective.articles.ArticleDataGateway;
+import io.collective.articles.ArticleInfo;
 import io.collective.restsupport.RestTemplate;
+import io.collective.rss.RSS;
 import io.collective.workflow.Worker;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 public class EndpointWorker implements Worker<EndpointTask> {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -31,7 +35,8 @@ public class EndpointWorker implements Worker<EndpointTask> {
         gateway.clear();
 
         { // todo - map rss results to an article infos collection and save articles infos to the article gateway
-
+            RSS rss = new XmlMapper().readValue(response, RSS.class);
+            rss.getChannel().getItem().forEach(item -> this.gateway.save(item.getTitle()));
         }
     }
 }
